174fdef97cec551bd9bfe7972537eaa4f96645b3
Mensch <mensch272@users.noreply.github.com>
2022-08-13 00:52:51 +0500

    feat: delete output folder after benchmark.

--- bench.sh
+++ bench.sh
@@ -3,9 +3,11 @@
 cargo build -p warpa --release
 
 echo "rust:warpa"
-time (./target/release/warpa x test.rpa)
+time (./target/release/warpa x test.rpa -o game)
 
 echo ""
 echo ""
 echo "python:rpatool"
-time (python rpatool.py -x test.rpa)
+time (python rpatool.py -x test.rpa -o game)
+
+rm -rf game
