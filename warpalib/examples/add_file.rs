1467e4bbb8ebdd4295584c804f3cec458c18bbc4
Mensch <mensch272@users.noreply.github.com>
2022-08-14 23:50:49 +0500

    feat: add add_file example.

---
+++ warpalib/examples/add_file.rs
@@ -0,0 +1,17 @@
+use std::{io::Cursor, path::Path};
+
+use warpalib::{RenpyArchive, RpaResult};
+
+fn main() -> RpaResult<()> {
+    // Open an in memory archive.
+    let mut archive = RenpyArchive::new();
+
+    // Add readme into archive.
+    archive.add_file(Path::new("README.md"));
+
+    // Write the current to a buffer.
+    let mut buffer = Cursor::new(Vec::new());
+    archive.flush(&mut buffer)?;
+
+    Ok(())
+}
