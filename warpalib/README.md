f71641d9363a735b873b0f0bb7d2c1970c1f5d30
Mensch <mensch272@users.noreply.github.com>
2022-08-15 00:09:54 +0500

    feat: add modify_archive example.

--- warpalib/README.md
+++ warpalib/README.md
@@ -1,16 +1,1 @@
-# rpalib
-
-rpalib is a library that implements renpy archive standard for reading and writing. The project aims to provide an easy-to-use abstraction over renpy archives.
-
-## Support
-
-| Version | Read | Write |
-| ------- | ---- | ----- |
-| 3.2     | Yes  | No    |
-| 3.0     | Yes  | Yes   |
-| 2.0     | Yes  | Yes   |
-| 1.0     | No   | No    |
-
-## License
-
-This library is licensed under [MIT License](LICENSE).
+../README.md
---
+++ warpalib/examples/modify_archive_.rs
@@ -0,0 +1,44 @@
+use std::{
+    fs::{self, File},
+    path::{Path, PathBuf},
+};
+
+use warpalib::{RenpyArchive, RpaError, RpaResult};
+
+fn main() -> RpaResult<()> {
+    // Assuming there is an archive named "archive.rpa" in current directory
+    let path = Path::new("archive.rpa");
+    let mut archive = RenpyArchive::open(path)?;
+
+    // Make a change to the archive.
+    archive.add_file(Path::new("README.md"));
+
+    // Saving and replacing the archive is a bit complicated since
+    // we cannot read from and write to the same file at the same time
+    // for data corruption reasons. So, first we write to a temp file.
+    let temp_path = Path::new("archive.rpa.temp");
+
+    // A new block is created for two reasons.
+    // 1. Drop handle to temp_file.
+    // 2. Capture any errors to ensure that next step is executed.
+    let result = {
+        // Create and open temp file.
+        let mut temp_file = File::create(temp_path)?;
+
+        // Write to the opened temp file.
+        archive.flush(&mut temp_file)?;
+
+        // Move and replace the archive with temp.
+        fs::rename(temp_path, path).map_err(|e| RpaError::Io(e))
+    };
+
+    // Delete temp file if it still exists for cleanup.
+    // The file may still be present if archive writing failed.
+    // or, if the file move failed.
+    if temp_path.exists() {
+        fs::remove_file(temp_path)?;
+    }
+
+    // While delayed, we should never discard a possible error,
+    result
+}
